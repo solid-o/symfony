0ee258d4e15e1f72035c4d95eb2f4d42f473371e
Alessandro Chitolina <alekitto@gmail.com>
2020-05-28 19:04:02 +0200

    extend DTO resolver to retrieve version from current request

---
+++ src/DTO/Resolver.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solido\Symfony\DTO;
+
+use Solido\DtoManagement\Finder\ServiceLocatorRegistryInterface;
+use Solido\DtoManagement\InterfaceResolver\Resolver as BaseResolver;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class Resolver extends BaseResolver
+{
+    private ?RequestStack $requestStack;
+
+    public function __construct(ServiceLocatorRegistryInterface $registry, ?RequestStack $requestStack = null)
+    {
+        parent::__construct($registry);
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve(string $interface, $version = null)
+    {
+        if ($version === null && $this->requestStack !== null) {
+            $request = $this->requestStack->getCurrentRequest();
+            $version = $request !== null ? $request->attributes->get('_version') : null;
+        }
+
+        return parent::resolve($interface, $version);
+    }
+}
