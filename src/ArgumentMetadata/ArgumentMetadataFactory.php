c606656f4004e3ec6b0b81fac3a008820314a740
Alessandro Chitolina <alekitto@gmail.com>
2023-04-02 23:56:15 +0200

    fix: fixed argument metadata factory with symfony 6.2

--- src/ArgumentMetadata/ArgumentMetadataFactory.php
+++ src/ArgumentMetadata/ArgumentMetadataFactory.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Solido\Symfony\ArgumentMetadata;
 
+use ReflectionFunctionAbstract;
+use ReflectionMethod;
 use Solido\DtoManagement\Proxy\ProxyInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadataFactoryInterface;
@@ -27,14 +29,16 @@ class ArgumentMetadataFactory implements ArgumentMetadataFactoryInterface
      *
      * @return ArgumentMetadata[]
      */
-    public function createArgumentMetadata($controller): array
+    public function createArgumentMetadata($controller, ?ReflectionFunctionAbstract $reflector = null): array
     {
         if (is_array($controller) && is_subclass_of($controller[0], ProxyInterface::class)) {
             $controller[0] = get_parent_class($controller[0]);
+            $reflector = new ReflectionMethod(...$controller);
         } elseif ($controller instanceof ProxyInterface) {
             $controller = [get_parent_class($controller), '__invoke'];
+            $reflector = new ReflectionMethod(...$controller);
         }
 
-        return $this->decorated->createArgumentMetadata($controller);
+        return $this->decorated->createArgumentMetadata($controller, $reflector);
     }
 }
