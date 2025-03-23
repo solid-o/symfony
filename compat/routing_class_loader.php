7bb19833ff25f3487937e17c065b4cf9b4bb395d
Alessandro Chitolina <alekitto@gmail.com>
2025-03-24 00:45:15 +0100

    fix: remove implicit nullable argument in attribute class loader

--- compat/routing_class_loader.php
+++ compat/routing_class_loader.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Symfony\Component\Routing\Loader {
     if (!class_exists(AttributeClassLoader::class)) {
         abstract class AttributeClassLoader extends AnnotationClassLoader {
-            public function __construct(string $env = null)
+            public function __construct(string|null $env = null)
             {
                 parent::__construct(null, $env);
             }
