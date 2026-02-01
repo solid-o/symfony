36f9c02d238d80ba10a3181d692369af4d736419
Alessandro Chitolina <alekitto@gmail.com>
2026-02-01 23:48:30 +0100

    fix: do not register serializer proxy subscribers if class or interfaces do not exist

--- src/Resources/config/serializer.php
+++ src/Resources/config/serializer.php
@@ -2,15 +2,32 @@
 
 declare(strict_types=1);
 
+// phpcs:disable SlevomatCodingStandard.ControlStructures.EarlyExit.EarlyExitNotUsed
+
 use Solido\Serialization\DTO\JmsSerializerProxySubscriber;
 use Solido\Serialization\DTO\KcsSerializerProxySubscriber;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container): void {
-    $container->services()
-        ->set(KcsSerializerProxySubscriber::class)
-            ->tag('kernel.event_subscriber')
+    $exists = static function (string $class): bool {
+        try {
+            new ReflectionClass($class);
 
-        ->set(JmsSerializerProxySubscriber::class)
+            return true;
+        } catch (Throwable) {
+            return false;
+        }
+    };
+
+    if ($exists(KcsSerializerProxySubscriber::class)) {
+        $container->services()
+            ->set(KcsSerializerProxySubscriber::class)
+            ->tag('kernel.event_subscriber');
+    }
+
+    if ($exists(JmsSerializerProxySubscriber::class)) {
+        $container->services()
+            ->set(JmsSerializerProxySubscriber::class)
             ->tag('jms_serializer.event_subscriber');
+    }
 };
